b1382f40bffc72ca556ca00d1857f4a7e0cc8aea
TheMusicBoy <TheMusicBoy666@mail.ru>
2025-02-07 15:05:08 +0300

    Upgrade

--- api/ollama.py
+++ api/ollama.py
@@ -2,29 +2,78 @@ import requests
 import sys
 import json
 import click
+import os
+import copy
+import re
 
 import marshal
 
-class OllamaApi:
-    def __init__(self, address, port, chat_file="chats_data.bin"):
-        self.url = f"https://{address}:{port}/api/"
-        self.chat_file = chat_file
-        self.chat_data = {}
-        # self.load()
 
-    # def __def__(self):
-    #     self.dump()
+class OllamaApi:
+    def __init__(self, config_path):
+        self.config = self.get_config(config_path)
+        self.url = f"https://{self.config['ollama_endpoint']}/api/"
+        self.load()
+
+    def __del__(self):
+        self.dump()
+
+    def extract_think_text(self, text):
+        match = re.search(r'<think>(.*?)</think>', text, re.DOTALL)
+        if match:
+            think_text = match.group(1).strip()
+            
+            # Разделяем текст на части вне тегов <think>
+            parts = re.split(r'<think>.*?</think>', text, flags=re.DOTALL)
+            non_think_text = "".join(part.strip() for part in parts if part.strip())
+            
+            return think_text, non_think_text
+        else:
+            return None, text.strip()
+
+    def concat_think_text(self, think, text):
+        if think:
+            return f"<think>\n{think}\n</think>\n{text}"
+        else:
+            return text
+
+    def get_config(self, config_dir):
+        try:
+            os.makedirs(config_dir)
+        except FileExistsError:
+            pass
+
+        config_path = os.path.join(config_dir, 'config.json')
+
+        try:
+            with open(config_path, 'r') as file:
+                return json.load(file)
+        except Exception:
+            with open(config_path, 'w+') as file:
+                data = {
+                    'ollama_endpoint': '89.169.145.6:8080',
+                    'chat_data_path': os.path.join(config_dir, 'chat_data.bin'),
+                }
+                json.dump(data, file)
+                return data
 
     def get_url(self, method):
         return self.url + method
 
     def load(self):
-        with open(self.chat_file, "rb+") as f:
-            self.chat_data = marshal.load(f)
+        try:
+            with open(self.config['chat_data_path'], "rb") as file:
+                self.chat_data = marshal.load(file)
+        except FileNotFoundError:
+            self.chat_data = {}
+        except TypeError:
+            self.chat_data = {}
+        except ValueError:
+            self.chat_data = {}
 
     def dump(self):
-        with open(self.chat_file, "wb+") as f:
-            marshal.dump(self.chat_data, f)
+        with open(self.config['chat_data_path'], "wb+") as file:
+            marshal.dump(self.chat_data, file)
 
     def generate(self, model, prompt, options={}):
         try:
@@ -53,7 +102,7 @@ class OllamaApi:
                             try:
                                 json_data = json.loads(buffer)
                                 if 'response' in json_data:
-                                    return json_data['response']
+                                    return self.extract_think_text(json_data['response'])
                                 buffer = b""
                             except json.JSONDecodeError:
                                 continue
@@ -78,6 +127,27 @@ class OllamaApi:
 
                 chunks = response.iter_content(chunk_size=1024)
                 
+                def process(content, stage):
+                    if stage == 0:
+                        if content == '<think>':
+                            content = ""
+                            stage += 1
+                    elif stage == 1:
+                        content = re.sub(r"^[\s\n]*", "", content)
+                        if content:
+                            stage += 1
+                    elif stage == 2:
+                        if content == '</think>':
+                            content = ""
+                            stage += 1
+                    elif stage == 3:
+                        content = re.sub(r"^[\s\n]*", "", content)
+                        if content:
+                            stage += 1
+
+                    return content, stage
+
+                stage = 0
                 buffer = b""
                 for chunk in chunks:
                     if not chunk:
@@ -88,7 +158,8 @@ class OllamaApi:
                             try:
                                 json_data = json.loads(buffer)
                                 if 'response' in json_data:
-                                    yield json_data['response']
+                                    text, stage = process(json_data['response'], stage)
+                                    yield text, stage > 2
                                 if 'done' in json_data and json_data['done']:
                                     return
                                 buffer = b""
@@ -98,95 +169,185 @@ class OllamaApi:
         except requests.exceptions.RequestException as e:
             print(f"Error: {e}")
 
-    # def chat(self, chat_id, model, prompt, options={}):
-    #     try:
-    #         messages = self.chat_data.get(chat_id, [])
-    #         messages.append({
-    #             "role": "user",
-    #             "content": prompt
-    #         })
+    def chat(self, chat_id, model, prompt, options={}):
+        try:
+            messages = self.chat_data.get(chat_id, [])
+            messages.append({
+                "role": "user",
+                "think": None,
+                "text": prompt
+            })
 
-    #         with requests.post(
-    #             self.get_url("chat"),
-    #             headers={"Content-Type": "application/json"},
-    #             json={
-    #                 "model": model,
-    #                 "messages": messages,
-    #                 "options": options,
-    #                 "stream": False
-    #             },
-    #             stream=True
-    #         ) as response:
-    #             response.raise_for_status()
+            parsed = copy.deepcopy(messages)
+            for message in parsed:
+                message['content'] = self.concat_think_text(message['think'], message['text'])
+                del message['think']
+                del message['text']
 
-    #             chunks = response.iter_content(chunk_size=1024)
+            with requests.post(
+                self.get_url("chat"),
+                headers={"Content-Type": "application/json"},
+                json={
+                    "model": model,
+                    "messages": parsed,
+                    "options": options,
+                    "stream": False
+                },
+                stream=True
+            ) as response:
+                response.raise_for_status()
 
-    #             buffer = b""
-    #             for chunk in chunks:
-    #                 if not chunk:
-    #                     continue
-    #                 for line in chunk.split(b'\r\n'):
-    #                     if line:
-    #                         buffer += line
-    #                         try:
-    #                             json_data = json.loads(buffer)
-    #                             if 'message' in json_data:
-    #                                 messages.append(json_data['message'])
-    #                                 self.chat_data[chat_id] = messages
-    #                                 return json_data['message']['content']
-    #                             buffer = b""
-    #                         except json.JSONDecodeError:
-    #                             continue
+                chunks = response.iter_content(chunk_size=1024)
 
-    #     except requests.exceptions.RequestException as e:
-    #         print(f"Error: {e}")
+                buffer = b""
+                for chunk in chunks:
+                    if not chunk:
+                        continue
+                    for line in chunk.split(b'\r\n'):
+                        if line:
+                            buffer += line
+                            try:
+                                json_data = json.loads(buffer)
+                                if 'message' in json_data:
+                                    content = json_data['message']['content']
+                                    think, text = self.extract_think_text(content)
+                                    messages.append({
+                                        "role": "user",
+                                        "think": think,
+                                        "text": text
+                                    })
+                                    self.chat_data[chat_id] = messages
+                                    return think, text
+                                buffer = b""
+                            except json.JSONDecodeError:
+                                continue
 
-    # def chat_stream(self, chat_id, model, prompt, options={}):
-    #     try:
-    #         messages = self.chat_data.get(chat_id, [])
-    #         messages.append({
-    #             "role": "user",
-    #             "content": prompt
-    #         })
+        except requests.exceptions.RequestException as e:
+            print(f"Error: {e}")
 
-    #         with requests.post(
-    #             self.get_url("chat"),
-    #             headers={"Content-Type": "application/json"},
-    #             json={
-    #                 "model": model,
-    #                 "messages": messages,
-    #                 "options": options,
-    #                 "stream": True
-    #             },
-    #             stream=True
-    #         ) as response:
-    #             response.raise_for_status()
+    def chat_stream(self, chat_id, model, prompt, options={}):
+        try:
+            messages = self.chat_data.get(chat_id, [])
+            messages.append({
+                "role": "user",
+                "think": None,
+                "text": prompt
+            })
 
-    #             chunks = response.iter_content(chunk_size=1024)
-    #             
-    #             message = {
-    #                 "role": "assistent",
-    #                 "content": ""
-    #             }
-    #             buffer = b""
-    #             for chunk in chunks:
-    #                 if not chunk:
-    #                     continue
-    #                 for line in chunk.split(b'\r\n'):
-    #                     if line:
-    #                         buffer += line
-    #                         try:
-    #                             json_data = json.loads(buffer)
-    #                             if 'response' in json_data:
-    #                                 message['content'] += json_data['message']['content']
-    #                                 yield json_data['message']['content']
-    #                             if 'done' in json_data and json_data['done']:
-    #                                 messages.append(message)
-    #                                 self.chat_data[chat_id] = messages
-    #                                 return
-    #                             buffer = b""
-    #                         except json.JSONDecodeError:
-    #                             continue
+            parsed = copy.deepcopy(messages)
+            for message in parsed:
+                message['content'] = self.concat_think_text(message.get('think', ''), message['text'])
+                del message['think']
+                del message['text']
 
-    #     except requests.exceptions.RequestException as e:
-    #         print(f"Error: {e}")
+            with requests.post(
+                self.get_url("chat"),
+                headers={"Content-Type": "application/json"},
+                json={
+                    "model": model,
+                    "messages": parsed,
+                    "options": options,
+                    "stream": True
+                },
+                stream=True
+            ) as response:
+                response.raise_for_status()
+
+                chunks = response.iter_content(chunk_size=1024)
+                
+                message = {
+                    "role": "assistent",
+                    "think": "",
+                    "text": ""
+                }
+
+                def process(content, stage):
+                    if stage == 0:
+                        if content == '<think>':
+                            content = ""
+                            stage += 1
+                    elif stage == 1:
+                        content = re.sub(r"^[\s\n]*", "", content)
+                        if content:
+                            stage += 1
+                    elif stage == 2:
+                        if content == '</think>':
+                            content = ""
+                            stage += 1
+                    elif stage == 3:
+                        content = re.sub(r"^[\s\n]*", "", content)
+                        if content:
+                            stage += 1
+
+                    return content, stage
+
+                stage = 0
+                buffer = b""
+                for chunk in chunks:
+                    if not chunk:
+                        continue
+                    for line in chunk.split(b'\r\n'):
+                        if line:
+                            buffer += line
+                            try:
+                                json_data = json.loads(buffer)
+                                if 'message' in json_data:
+                                    text, stage = process(json_data['message']['content'], stage)
+
+                                    if stage < 2:
+                                        message['think'] += text
+                                    else:
+                                        message['text'] += text
+                    
+                                    buffer = b""
+                                    yield text, stage > 2
+                                if 'done' in json_data and json_data['done']:
+                                    messages.append(message)
+                                    self.chat_data[chat_id] = messages
+                                    return
+                                buffer = b""
+                            except json.JSONDecodeError:
+                                continue
+
+        except requests.exceptions.RequestException as e:
+            print(f"Error: {e}")
+
+    def delete_chat(self, chat_id):
+        if chat_id not in self.chat_data:
+            print(f"No chat with name '{chat_id}'")
+        else:
+            del self.chat_data[chat_id]
+
+    def chat_from_message(self, chat_id, message_id, model, prompt, options={}):
+        if chat_id not in self.chat_data:
+            print(f"No chat with name '{chat_id}'")
+
+        if message_id >= len(self.chat_data[chat_id]):
+            print(f"No message {chat_id} in chat '{chat_id}'")
+
+        messages = self.chat_data.get(chat_id)[:message_id]
+
+        return self.chat(chat_id, model, prompt, options)
+
+    def chat_from_message_stream(self, chat_id, message_id, model, prompt, options={}):
+        if chat_id not in self.chat_data:
+            print(f"No chat with name '{chat_id}'")
+
+        if message_id >= len(self.chat_data[chat_id]):
+            print(f"No message {chat_id} in chat '{chat_id}'")
+
+        messages = self.chat_data.get(chat_id)[:message_id]
+
+        for token in self.chat_stream(chat_id, model, prompt, options):
+            yield token
+
+    def chat_list(self):
+        result = []
+
+        for chat_name, message in self.chat_data.items():
+            result.append(chat_name)
+
+        return result
+
+    def get_chat(self, chat_id):
+        return self.chat_data.get(chat_id, [])
--- main.py
+++ main.py
@@ -7,28 +7,138 @@ import sys
 
 from api.ollama import OllamaApi
 
-DEFAULT_ADDRESS = "89.169.145.6"
-DEFAULT_PORT = 8080
 
-@click.command()
-@click.option('-p', '--port', default=DEFAULT_PORT, help="server port")
-@click.option('-a', '--address', default=DEFAULT_ADDRESS, help="server address")
-@click.option('-m', '--model', default="dpsk14", help="model <dpsk7, dpsk14, dpsk32>")
-@click.option('--stream/--no-stream', default=True, help="print stream or whole message")
-@click.argument('prompt')
-def cli(port, address, model, stream, prompt):
+DEFAULT_CONFIG_PATH = "/home/painfire/.config/dpsk"
+
+
+@click.group()
+def cli():
+    """
+    """
+    pass
+
+
+model_option = click.option(
+    '-m',
+    '--model',
+    default="dpsk14",
+    help="model <dpsk7, dpsk14, dpsk32>"
+)
+
+
+stream_option = click.option(
+    '--stream/--no-stream',
+    default=True,
+    help="print stream or whole message"
+)
+
+
+think_option = click.option(
+    '--think/--no-think',
+    default=True,
+    help="Print think"
+)
+
+
+prompt_argument = click.argument(
+    'prompt',
+    nargs=-1
+)
+
+
+@cli.command(name='generate', help='generate single text by prompt')
+@model_option
+@stream_option
+@think_option
+@prompt_argument
+def do_generate(model, stream, think, prompt):
+    prompt = ' '.join(prompt)
+
     if not sys.stdin.isatty():
         prompt += "\n\n"
         for line in sys.stdin:
             prompt += line
 
-    ollama = OllamaApi(address, port)
+    ollama = OllamaApi(DEFAULT_CONFIG_PATH)
+
     if (stream):
-        for token in ollama.generate_stream(model, prompt):
-            print(token, end="", flush=True)
+        text_mode = True
+        for token, talking in ollama.generate_stream(model, prompt):
+            if text_mode != talking:
+                if think:
+                    if talking:
+                        print('\n--- Printing result... ---')
+                    else:
+                        print('--- Thinking... ---')
+                text_mode = talking
+
+            if think or talking:
+                print(token, end="", flush=True)
         print(flush=True)
     else:
-        print(ollama.generate(model, prompt), flush=True)
+        think, text = ollama.generate(model, prompt)
+        print(f"--- Thinking... ---\n{think}\n--- Printing result... ---\n{text}", flush=True)
+
+
+chat_id_option = click.option(
+    '-c',
+    '--chat-id',
+    default="First chat",
+    help="chat name"
+)
+
+
+@cli.command(name='chat', help='chat with bot')
+@chat_id_option
+@model_option
+@stream_option
+@think_option
+@prompt_argument
+def do_chat(chat_id, model, stream, think, prompt):
+    chat_id = chat_id.strip()
+    prompt = ' '.join(prompt)
+
+    if not sys.stdin.isatty():
+        prompt += "\n\n"
+        for line in sys.stdin:
+            prompt += line
+
+    ollama = OllamaApi(DEFAULT_CONFIG_PATH)
+
+    if (stream):
+        text_mode = True
+        for token, talking in ollama.chat_stream(chat_id, model, prompt):
+            if text_mode != talking:
+                if think:
+                    if talking:
+                        print('\n--- Printing result... ---')
+                    else:
+                        print('--- Thinking... ---')
+                text_mode = talking
+
+            if think or talking:
+                print(token, end="", flush=True)
+        print(flush=True)
+    else:
+        think, text = ollama.chat(chat_id, model, prompt)
+        print(f"--- Thinking... ---\n{think}\n--- Printing result... ---\n{text}", flush=True)
+
+
+@cli.command(name='list', help='list of chats')
+def do_list():
+    ollama = OllamaApi(DEFAULT_CONFIG_PATH)
+    for chat_name in ollama.chat_list():
+        print(chat_name)
+
+chat_id_argument = click.argument('chat_id', nargs=1)
+
+
+@cli.command(name='delete', help='delete chat')
+@chat_id_argument
+def do_delete(chat_id):
+    ollama = OllamaApi(DEFAULT_CONFIG_PATH)
+    ollama.delete_chat(chat_id)
+
 
 if __name__ == "__main__":
     cli()
